6be9c371b37c398e3ce26b339a3982bb2d4674d7
tracey brimage <onelovenilynn@gmail.com>
2019-09-04 20:47:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,14 +2,14 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-def introduction (Tracey)
+def introduction (tracey)
   "Hi, my name is #(name)"
 end
 
-def introduction_with_language(Tracey,language)
+def introduction_with_language(tracey,language)
   'Hi, my name is #{Tracey}" and Iam learning to program in #{language).'
 end
-def introduction_with_language_optional(Tracey, language="Ruby")
+def introduction_with_language_optional(tracey, language="Ruby")
   "Hi, my name is #{Tracey}" and Iam learning to program in #(language).'
 end
 
